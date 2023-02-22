83078010f18a6c0d8527922183f58daf4624c8cb
Oleksii <al.dimitrash@gmail.com>
2023-02-22 18:14:46 +0200

    update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightboxRef = document.querySelector('.gallery');
+const lightboxMarkup = createBoxItem(galleryItems);
+lightboxRef.insertAdjacentHTML(`afterbegin`, lightboxMarkup);
+
+function createBoxItem(galleryItems) {
+    return galleryItems
+        .map(
+            ({ preview, original, description }) =>
+                `<a class="gallery__item" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>`
+        )
+        .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
---
+++ src/js/02-video.js
@@ -0,0 +1,17 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+const iframe = document.getElementById("vimeo-player");
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(e) {
+    console.log(e.seconds);
+    const time = e.seconds;
+    localStorage.setItem("videoplayer-current-time", time);
+};
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+const currentTame = localStorage.getItem("videoplayer-current-time");
+const startTame = JSON.parse(currentTame);
+console.log(startTame);
+player.setCurrentTime(currentTame || 0);
